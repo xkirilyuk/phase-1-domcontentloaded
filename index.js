add53cbb527bccfd7ff541f56a6241401d27d7e9
Xenia <kakirilyuk@gmail.com>
2022-06-15 22:56:35 +0200

    commit tree 334c2627bf366f3fe5ca16f6e967a75ba6cc3f80

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+const event = document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("text").textContent = "This is really cool!"
+
+
+});
+
+console.log('Hello');
+
+
+//const textChange = document.getElementById("p").textContent = 
+
+//textChange.textContent = textChange.textContent.replace("JavaScript is so cool. It lets me add text to my page programmatically.", "This is really cool!");
+
+
+//textChange.textContent = "This is really cool!";
+
+
+//const textChange = document.getElementById('p').innerHTML = 'This is really cool!';
